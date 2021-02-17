de9b74066486203e333753e17d7e8baf2d94c434
Nickolay Lyman <kolyalyman@gmail.com>
2021-02-17 10:39:29 +0200

    Перенес json файлы в папку data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-import user from './user.json';
-import stats from './statistical-data.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
+import user from './data/user.json';
+import stats from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
